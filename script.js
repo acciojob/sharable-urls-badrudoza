4ddc5262debec08a2ca177239d9fb10893a3c00a
Badru Doza Ansari <106438583+badrudoza@users.noreply.github.com>
2025-09-03 16:27:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 // your code here
+const urlDisplay=document.querySelector("#url");
+const formSubmit=document.querySelector("form");
+
+formSubmit.addEventListener("submit",(e)=>{
+	e.preventDefault();
+	let baseUrl=urlDisplay.textContent;
+	console.log(baseUrl);
+	const name=document.querySelector("#name");
+	const year=document.querySelector("#year");
+	let queryParam=[];
+	if(name){
+		queryParam.push("name="+encodeURIComponent(name));
+	}
+	if(year){
+		queryParam.push("year="+encodeURIComponent(year));
+	}
+	if(queryParam.length>0){
+		baseUrl+="?"+queryParam.join("&");
+	}
+	urlDisplay.textContent=baseUrl;
+});
+
